90034c19052aa77b81f599030ff7069631e50c3a
samiahlfors <hello@samiahlfors.com>
2022-11-04 15:53:07 +0100

    Character movement added to project

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,21 +1,29 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
-    #region Variables
-
     private Vector2 _input;
+    private CharacterController _characterController;
+    private Vector3 _direction;
 
-    #endregion
+    [SerializeField] private float speed;
 
-    #region Events
+    private void Awake()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        _characterController.Move(_direction * speed * Time.deltaTime);
+    }
 
     public void Move(InputAction.CallbackContext context)
     {
         _input = context.ReadValue<Vector2>();
-        Debug.Log(_input);
+        _direction = new Vector3(_input.x, 0.0f, _input.y);
     }
-
-    #endregion
 }
