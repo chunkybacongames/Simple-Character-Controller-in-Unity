1a047e289143250917f4b83c0fe5da312508aa2c
samiahlfors <hello@samiahlfors.com>
2022-11-12 19:08:37 +0100

    Gravity added to Character Controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,14 +5,28 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
+    #region Variables: Movement
+
     private Vector2 _input;
     private CharacterController _characterController;
     private Vector3 _direction;
 
+    [SerializeField] private float speed;
+
+    #endregion
+    #region Variables: Rotation
+
     [SerializeField] private float smoothTime = 0.05f;
     private float _currentVelocity;
-    
-    [SerializeField] private float speed;
+
+    #endregion
+    #region Variables: Gravity
+
+    private float _gravity = -9.81f;
+    [SerializeField] private float gravityMultiplier = 3.0f;
+    private float _velocity;
+
+    #endregion
 
     private void Awake()
     {
@@ -21,12 +35,36 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        ApplyGravity();
+        ApplyRotation();
+        ApplyMovement();
+    }
+
+    private void ApplyGravity()
+    {
+        if (_characterController.isGrounded && _velocity < 0.0f)
+        {
+            _velocity = -1.0f;
+        }
+        else
+        {
+            _velocity += _gravity * gravityMultiplier * Time.deltaTime;
+        }
+        
+        _direction.y = _velocity;
+    }
+    
+    private void ApplyRotation()
+    {
         if (_input.sqrMagnitude == 0) return;
         
         var targetAngle = Mathf.Atan2(_direction.x, _direction.z) * Mathf.Rad2Deg;
         var angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _currentVelocity, smoothTime);
         transform.rotation = Quaternion.Euler(0.0f, angle, 0.0f);
-        
+    }
+
+    private void ApplyMovement()
+    {
         _characterController.Move(_direction * speed * Time.deltaTime);
     }
 
